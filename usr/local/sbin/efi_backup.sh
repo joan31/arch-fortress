83399587ee0c933b9806d5c3d3d72eeaab1e93bc
Joan <12939441+joan31@users.noreply.github.com>
2025-06-12 09:27:18 +0200

    Update efi_backup.sh

--- usr/local/sbin/efi_backup.sh
+++ usr/local/sbin/efi_backup.sh
@@ -2,5 +2,5 @@
 ## SCRIPT EFI BACKUP
 ## /usr/local/sbin/efi_backup.sh
 
-tar -czf "/.efibackup/efi-$(date +%Y%m%d-%H%M%S).tar.gz" -C / efi;
+tar -czf "/.efibackup/efi-$(date +%Y%m%d-%H%M%S).tar.gz" -C / efi
 ls -1t /.efibackup/efi-*.tar.gz | tail -n +4 | xargs -r rm --
